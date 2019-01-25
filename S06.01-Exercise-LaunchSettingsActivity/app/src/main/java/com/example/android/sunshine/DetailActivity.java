2a3d89b34a0fa584466ea452b147d12559410e51
Jose Javier Aviles Gomez <jjaviles2010@gmail.com>
2019-01-24 22:00:36 -0200

    Configuring Settings screen

--- S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -55,5 +55,18 @@ public class DetailActivity extends AppCompatActivity {
         return true;
     }
 
-    // TODO (7) Launch SettingsActivity when the Settings option is clicked
+    // COMPLETED (7) Launch SettingsActivity when the Settings option is clicked
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id == R.id.action_settings){
+            Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
